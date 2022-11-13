56cc3a9d874b10d18e54eff7a85f4335eb59a092
ArtemHorbas <artemgorbas95@gmail.com>
2022-11-13 18:59:47 +0100

    sequelize setup

---
+++ src/config/sequelize.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config'
+import { SequelizeModuleOptions } from '@nestjs/sequelize'
+
+export const getSequelizeConfig = async (
+	configService: ConfigService
+): Promise<SequelizeModuleOptions> => ({
+	dialect: 'postgres',
+	host: configService.get('DB_HOST'),
+	port: configService.get('DB_PORT'),
+	username: configService.get('DB_USER'),
+	password: configService.get('DB_PASSWORD'),
+	database: configService.get('DB_NAME'),
+	synchronize: true,
+	autoLoadModels: true,
+	models: []
+})
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from '../user/user.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { getSequelizeConfig } from '../../config/sequelize.config'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true
+		}),
+		SequelizeModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getSequelizeConfig
 		}),
 		UserModule
 	],
