81247e41afa29abcca7c58a37f9ad6f85cce2651
ArtemHorbas <artemgorbas95@gmail.com>
2022-11-14 21:31:38 +0100

    decorators

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service'
 import { CreateUserDto } from '../user/dto/create-user.dto'
 import { AuthResponse } from './response/auth.response'
 import { UserLoginDto } from './dto/user-login.dto'
+import { Auth } from '../../decorators/auth.decorator'
+import { User } from '../../decorators/user.decorator'
+import { UserTable } from '../user/models/user.model'
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +19,11 @@ export class AuthController {
 	@Post('login')
 	login(@Body() dto: UserLoginDto): Promise<AuthResponse> {
 		return this.authService.loginUser(dto)
+	}
+
+	@Post('test')
+	@Auth()
+	test(@User() user: UserTable) {
+		return user
 	}
 }
